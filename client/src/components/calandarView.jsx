49be912d559142052e1e02a255ebc5d3b4851513
Christopher Desmond <chrisdesmond841@gmail.com>
2024-01-30 14:43:55 -0500

    updated calandar view data

--- client/src/components/calandarView.jsx
+++ client/src/components/calandarView.jsx
@@ -1,10 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import axios from 'axios';
 import './calandarView.css';
-
-
-
-
 import CalendarHeatmap from 'react-calendar-heatmap';
 import 'react-calendar-heatmap/dist/styles.css';
 
@@ -15,41 +11,52 @@ const generateDateValues = (habitDates, startDate, endDate) => {
 
   while (currentDate <= endDate) {
     const dateString = currentDate.toISOString().split('T')[0];
-    const count = habitDates?.filter(habitDate => habitDate === dateString).length || 0;
+    const count =
+      habitDates?.find((habitDate) => habitDate.date === dateString)?.count || 0;
     dateValues.push({ date: dateString, count });
     currentDate.setDate(currentDate.getDate() + 1);
   }
 
+  console.log('Generated Date Values:', dateValues); // Added console.log
   return dateValues;
 };
 
 const CalendarView = ({ habits }) => {
-    const startDate = new Date('2024-01-01');
-    const endDate = new Date('2024-12-31');
-  
-    return (
-      <div>
-        {habits.map((habit) => (
-          <div key={habit._id} className="habit-calendar">
-            <h2>
-              {habit.habit_name}
-              {habit.emoji}
-            </h2>
-            <CalendarHeatmap
-              key={habit._id}
-              startDate={startDate}
-              endDate={endDate}
-              values={[
-                { values: generateDateValues(habit.daily_check, startDate, endDate) },
-              ]}
-              showWeekdayLabels={true}
-              showOutOfRangeDays={true}
-            />
-          </div>
-        ))}
-      </div>
-    );
-  };
-  
-  export default CalendarView;
-  
+  const startDate = new Date('2024-01-01');
+  const endDate = new Date('2024-12-31');
+
+  return (
+    <div>
+      {habits.map((habit) => (
+        <div key={habit._id} className="habit-calendar">
+          <h2>
+            {habit.habit_name}
+            {habit.emoji}
+          </h2>
+          <CalendarHeatmap
+            key={habit._id}
+            startDate={startDate}
+            endDate={endDate}
+            values={generateDateValues(habit.daily_check, startDate, endDate)}
+            classForValue={value => {
+                if (!value || value === 0) {
+                  return 'color-empty';
+                }
+                return `color-github-${value.count}`;
+              }}
+            showWeekdayLabels={true}
+            showOutOfRangeDays={true}
+            tooltipDataAttrs={(value) => {
+              return {
+                'data-tip': `${value.date} has count: ${value.count}`,
+              };
+              
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CalendarView;
