02f082e46c01e30b81cfa4a55cae2d37b4232b2a
Christopher Desmond <chrisdesmond841@gmail.com>
2024-02-02 15:37:55 -0500

    maxxed out completions to 5 per day

--- client/src/components/habitComponent.jsx
+++ client/src/components/habitComponent.jsx
@@ -9,7 +9,7 @@ const HabitComponent = ({ habits, handleClosePopups }) => {
 
   const updateDailyCheck = async (habitId, habitDates, date, event) => {
     event.preventDefault();
-
+  
     try {
       // Find the daily check data for today
       const todayDate = date.toISOString().split('T')[0];
@@ -18,35 +18,40 @@ const HabitComponent = ({ habits, handleClosePopups }) => {
       // Get the count value for today
       const currentCountValue = todayCheck ? todayCheck.count : 0;
       setCountValue(currentCountValue);
-
+  
       // Log the current count value for today
       console.log('Current Count Value for Today:', currentCountValue);
-
-      // Increment the count value
-      const updatedCountValue = currentCountValue + 1;
-      setCountValue(updatedCountValue);
-
-      console.log(updatedCountValue)
-
-      // Format date to match db
-      const formattedDate = date.toISOString().split('T')[0];
-      const updateDailyValue = { date: formattedDate };
-
-      console.log(updateDailyValue)
-
-      const newDailycheckObj = {
-        date: formattedDate,
-        count: updatedCountValue,
-      };
-     
-      //send post request back for todays date count + 1
-      await axios.post(`/update-daily-check/${habitId}`, newDailycheckObj);
-
-
-      // Success
-      console.log('Habit Completed for the day');
-
-      toast.success('Habit Completed for the day!');
+  
+      // Check if the count is less than 5
+      if (currentCountValue < 5) {
+        // Increment the count value
+        const updatedCountValue = currentCountValue + 1;
+        setCountValue(updatedCountValue);
+  
+        console.log(updatedCountValue);
+  
+        // Format date to match db
+        const formattedDate = date.toISOString().split('T')[0];
+        const updateDailyValue = { date: formattedDate };
+  
+        console.log(updateDailyValue);
+  
+        const newDailycheckObj = {
+          date: formattedDate,
+          count: updatedCountValue,
+        };
+       
+        // Send post request back for today's date count + 1
+        await axios.post(`/update-daily-check/${habitId}`, newDailycheckObj);
+  
+        // Success
+        console.log('Habit Completed for the day');
+        toast.success('Habit Completed for the day!');
+      } else {
+        console.log('Max Completions of 5 reached');
+        toast.success('Max Completions of 5 reached');
+      }
+  
       handleClosePopups();
     } catch (error) {
       // Send error for error
