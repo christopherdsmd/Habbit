cf39451d64bd45aa719828643f9fd075632f9de7
Christopher Desmond <104523163+christopherdsmd@users.noreply.github.com>
2024-05-28 15:31:45 -0400

    Update userContext.jsx comments

--- client/context/userContext.jsx
+++ client/context/userContext.jsx
@@ -2,18 +2,19 @@ import axios from 'axios';
 import { createContext, useState, useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 
+//exports user context data from to dashboard
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
-  const [user, setUser] = useState(null);
-  const location = useLocation();
+  const [user, setUser] = useState(null);  //user data
+  const location = useLocation();    
   
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const { data } = await axios.get('/profile');
+        const { data } = await axios.get('/profile');    //get request profile data 
         setUser(data);
-      } catch (error) {
+      } catch (error) {  //error catch
         console.error('Error fetching profile:', error.response?.data || error.message);
       }
     };
